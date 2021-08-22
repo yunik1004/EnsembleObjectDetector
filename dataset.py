942981a90777e0728db88b216511caea386bf451
yunik1004 <inkyuhak@gmail.com>
2021-08-22 10:24:55 +0900

    Add comments

--- dataset.py
+++ dataset.py
@@ -5,6 +5,19 @@ from mmdet.datasets import build_dataloader, build_dataset
 
 
 def gen_val_data_loader(config_path: str):
+    """
+    Generates the validation dataloader from the config.
+
+    Parameters
+    ----------
+    config_path : str
+        Path of the config
+
+    Returns
+    -------
+    DataLoader
+        Pytorch dataloader
+    """
     cfg = Config.fromfile(config_path)
     dataset = build_dataset(cfg.data.val)
     data_loader = build_dataloader(
--- detector.py
+++ detector.py
@@ -6,6 +6,10 @@ from mmcv.parallel import MMDataParallel
 
 
 class ObjectDetector:
+    """
+    Object detector model
+    """
+
     def __init__(
         self,
         config_path: str,
@@ -13,6 +17,21 @@ class ObjectDetector:
         output_path: str,
         device: str = "cuda:0",
     ) -> None:
+        """
+        Constructor of ObjectDetector class.
+        It generates the pretrained Pytorch model based on the config and checkpoint.
+
+        Parameters
+        ----------
+        config_path : str
+            Path of the config
+        checkpoint_path : str
+            Path of the checkpoint
+        output_path : str
+            Path of the output
+        device : str, optional
+            Device type. It should be "cpu" or "cuda:xxx", by default "cuda:0"
+        """
         model = init_detector(config_path, checkpoint_path, device=device)
         if device == "cpu":
             self._model = MMDataParallel(model)
@@ -23,6 +42,20 @@ class ObjectDetector:
         self._output_path = output_path
 
     def inference(self, data_loader) -> List[Any]:
+        """
+        Returns the list of inference results for the given data loader.
+        It also saves the results into the output file.
+
+        Parameters
+        ----------
+        data_loader : DataLoader
+            Pytorch dataloader
+
+        Returns
+        -------
+        List[Any]
+            List of inference results
+        """
         if os.path.isfile(self._output_path):
             result = load(self._output_path)
         else:
@@ -33,10 +66,37 @@ class ObjectDetector:
 
 
 class EnsembleObjectDetector:
+    """
+    Object detector model which uses the ensemble method
+    """
+
     def __init__(self, detectors: List[ObjectDetector]) -> None:
+        """
+        Constructor of EnsembleObjectDetector class.
+        It stores ObjectDetector objects which will be used for the ensemble model.
+
+        Parameters
+        ----------
+        detectors : List[ObjectDetector]
+            List of ObjectDetector objects which are the part of ensemble model
+        """
         self._detectors = detectors
 
     def inference(self, data_loader) -> List[Any]:
+        """
+        Return the inference results.
+        It also saves the results into the output file.
+
+        Parameters
+        ----------
+        data_loader : DataLoader
+            Pytorch dataloader
+
+        Returns
+        -------
+        List[Any]
+            List of ensemble inference results
+        """
         results = list()
         for detector in self._detectors:
             result = detector.inference(data_loader)
